2d4975c5f440bebc83e3c823495148645b06c66b
Feathers <git@feath.rs>
2022-07-11 19:45:21 +0000

    Move stuff around

---
+++ src/data.rs
@@ -0,0 +1,12 @@
+// F-Chat commands follow a specific format:
+// XXX {...}
+// Being a 3 character command code, followed by JSON data
+// This is fine.
+
+enum ClientCommand {
+    
+}
+
+enum ServerCommand {
+
+}
