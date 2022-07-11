fa33e7f854707fbdb7fd7061e98617d8f59d2a43
Feathers <git@feath.rs>
2022-07-11 19:56:24 +0000

    Use ajacent tagging

---
+++ src/protocol.rs
@@ -0,0 +1,6 @@
+// For full ser/de of commands
+// ClientCommand can be serialized,
+// ServerCommand can be deserialized
+// But mutually they can suck one.
+
+// Use Serde to convert them to/from adjacent format
